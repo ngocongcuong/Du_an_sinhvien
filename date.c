d2f280cf41845875aea8ab5c225ffe7b69c8f52e
ngocongcuong <74588430+ngocongcuong@users.noreply.github.com>
2020-12-03 16:23:06 +0700

    cuong them printf

--- date.c
+++ date.c
@@ -3,10 +3,14 @@
 #include "date.h"
 void inputSV(SV *d)
 {
-	printf("\nNhap ma sinh vien, ten,gioi tinh,ngay sinh,que,lop:");
-	scanf("%d%s%s%d%s%s",&d->idsinhvien,&d->name,&d->gioitinh,&d->ngaysinh,&d->que,&d->lop);
+	printf("\nNhap ma sinh vien:\n");	scanf("%d",&d->idsinhvien);fflush(stdin);
+	printf("\nNhap ten sinh vien:\n");	gets(&d->name);
+	printf("\nNhap gioi tinh sinh vien:\n");	scanf("%s",&d->gioitinh);
+	printf("\nNhap ngay sinh sinh vien:\n");	scanf("%d",&d->ngaysinh);
+	printf("\nNhap lop sinh vien:\n");	scanf("%s",&d->lop);
+	printf("\nNhap que sinh vien:\n");	scanf("%s",&d->que);
 }
 void outputSV(SV d)
 {
-	printf("%d %s %s %d %s %s",d.idsinhvien,d.name,d.gioitinh,d.ngaysinh,d.que,d.lop);
+	printf("Ma sinh vien: %d\nTen: %s\nGioi tinh: %s\nNgay sinh %d\nLop: %s\nQue quan: %s ",d.idsinhvien,d.name,d.gioitinh,d.ngaysinh,d.lop,d.que);
 }
--- date.h
+++ date.h
@@ -3,10 +3,11 @@ typedef struct
 {
 	int idsinhvien;
 	char name[40];
-	char gioitinh;
+	char gioitinh[3];
 	int ngaysinh;
-	char que[30];
 	char lop[20];
+	char que[30];
+
 }SV;
 void inputSV(SV *d);
 void outputSV(SV d);
