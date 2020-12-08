00671d061c26f9528a4546486523185aaa7edf36
ngocongcuong <74588430+ngocongcuong@users.noreply.github.com>
2020-12-08 11:16:02 +0700

    sap xep

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <conio.h>
 #include <stdlib.h>
+#include <string.h>
+#define CURRENT_YEAR 2020
 struct Mark {
 	float math;
 	float physics;
 	float chemistry;
 };
 struct Student {
-	char name[100];
+	char name[150];
 	char province[100];
 	int year_of_birth;
 	//1 sinh vien co 1 diem
@@ -17,14 +20,32 @@ void input(struct Student* students, int numberOfStudents) {
 	int i;
 	for (i = 0; i < numberOfStudents; i++) {
 		struct Student* currentStudent = students + i;
-		printf("\nEnter student's name: "); fflush(stdin); gets(currentStudent->name);
+		printf("\nEnter student's name: "); gets(currentStudent->name);
+		while (1) {
+			if(gets(currentStudent->name) != NULL) break;
+		}
 		printf("\nEnter student's province: "); gets(currentStudent->province);
-		printf("\nEnter student's yeat of birth: "); scanf_s("%d",&currentStudent->year_of_birth);
+		// validate: age must be 18->45
+		do
+		{
+			printf("\nEnter student's yeat of birth: "); scanf_s("%d",&currentStudent->year_of_birth);
+		} while (CURRENT_YEAR - currentStudent->year_of_birth <17 
+			|| CURRENT_YEAR - currentStudent->year_of_birth >45);
 		printf("\nEnter student's mark: \n");
-		currentStudent->mark = (struct Mark*)malloc(sizeof(struct Mark*)); //alloc 1 object of Mark
-		printf("\nEnter your math: "); scanf_s("%f", &currentStudent->mark->math);
-		printf("\nEnter your physics: "); scanf_s("%f", &currentStudent->mark->physics);
-		printf("\nEnter your chemistry: "); scanf_s("%f", &currentStudent->mark->chemistry);
+		currentStudent->mark = (struct Mark*)malloc(sizeof(struct Mark)); //alloc 1 object of Mark
+		do
+		{
+			printf("\nEnter your math: "); scanf_s("%f", &currentStudent->mark->math);	
+		} while (currentStudent->mark->math >10 || currentStudent->mark->math <0);
+		do
+		{
+			printf("\nEnter your physics: "); scanf_s("%f", &currentStudent->mark->physics);
+		} while (currentStudent->mark->physics >10 || currentStudent->mark->physics < 0);
+		do
+		{
+			printf("\nEnter your chemistry: "); scanf_s("%f", &currentStudent->mark->chemistry);
+		} while (currentStudent->mark->chemistry >10 || currentStudent->mark->chemistry <0);
+		//diem tu 0 den 10
 	}
 	printResult(students, numberOfStudents);
 }
@@ -33,7 +54,8 @@ void printResult(struct Student* students, int numberOfStudents) {
 	for (i = 0; i < numberOfStudents; i++) {
 		struct Student* currentStudent = students + i;
 		printf("+----------------------------------------------------------------+\n");
-		printf("|%30c |%30c |%10d |%5.2f |%5.2f |%5.2f \n",
+		printf("|  Ten sinh vien   | Tinh, TP   | Nam sinh | Toan | Ly | Hoa \n");
+		printf("|%17s |%11s |%7d   |%4.1f  |%2.1f | %3.1f \n",
 				currentStudent->name, currentStudent->province,
 				currentStudent->year_of_birth, currentStudent->mark->math, currentStudent->mark->physics,
 				currentStudent->mark->chemistry
@@ -41,11 +63,29 @@ void printResult(struct Student* students, int numberOfStudents) {
 		printf("+----------------------------------------------------------------+\n");
 	}
 }
+void sort(struct Student* students, int numberOfStudents) {
+	int i,j;
+	for (i=0; i<numberOfStudents -1; i++) {
+		for (j=i+1; j<numberOfStudents; j++) {
+			struct Student* si = students +i;
+			struct Student* sj = students +j;
+			float totalI = si->mark->math + si->mark->physics + si->mark->chemistry;
+			float totalJ = sj->mark->math + sj->mark->physics + sj->mark->chemistry;
+			if (totalI<totalJ) {
+				//doi vi tri
+				struct Student temp = *si;
+				*si = *sj;
+				*sj = temp;
+			}
+		}
+	}
+	printResult(students, numberOfStudents);
+}
 
 int main() {
 	char choice;
 	int numberOfStudents;
-	struct Student* students;
+	struct Student* students = NULL;
 	do{
 	//creat a "console menu":
 	printf("+----------------------------------------------------------------+\n");
@@ -67,7 +107,10 @@ int main() {
 			input(students, numberOfStudents);
 			break;
 		case '2':
-			printf("Ban chon Sort\n");
+			//sap xep
+			if(students !=NULL && numberOfStudents >0) {
+				sort(students, numberOfStudents);
+			}
 			break;
 		case '3':
 			printf("Ban chon Analyze\n");
